e5125922772ef62e24f27ffd65e914114c7e11d8
chodkows <chodkows@gmail.com>
2023-03-07 18:10:19 +0100

    With channels with seat scenario

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,64 @@
-use std::sync::mpsc::{channel, Sender};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
+use std::{
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Barrier, Mutex,
+    },
+    thread,
+    time::Duration,
+};
+
+const EAT_TIME: Duration = Duration::from_secs(1);
+const THINK_TIME: Duration = Duration::from_secs(3);
+const EATING_COUNTER: usize = 3;
 
 struct Philosopher {
     name: String,
     left_fork: usize,
     right_fork: usize,
-    done: Sender<bool>,
+    done_channel: Sender<bool>,
 }
 
 impl Philosopher {
-    fn new(name: &str, left_fork: usize, right_fork: usize, done: Sender<bool>) -> Philosopher {
+    fn new(
+        name: &str,
+        left_fork: usize,
+        right_fork: usize,
+        done_channel: Sender<bool>,
+    ) -> Philosopher {
         Philosopher {
             name: name.to_string(),
             left_fork,
             right_fork,
-            done,
+            done_channel,
         }
     }
 
     fn done(&self) {
-        println!("{} is satisfied", self.name);
-        self.done
+        self.done_channel
             .send(true)
             .ok()
-            .expect("Unable to send message to channel");
+            .expect("Unable to send to done_channel");
     }
 
     fn eat(&self, table: &Table) {
-        let _left = table.forks[self.left_fork]
+        let _left_fork = table.forks[self.left_fork]
             .lock()
             .ok()
-            .expect("Unable to take left fork");
-        println!("        {} has left fork", self.name);
-        let _right = table.forks[self.right_fork]
+            .expect("Unable to lock left_fork mutex");
+        let _right_fork = table.forks[self.right_fork]
             .lock()
             .ok()
-            .expect("Unable to take right fork");
-        println!("        {} has right fork", self.name);
-        println!("{} is eating", self.name);
-        thread::sleep(Duration::from_secs(1));
-        println!("{} is done eating", self.name);
+            .expect("Unable to lock right_fork mutex");
+        println!("        {} started eating", self.name);
+        thread::sleep(EAT_TIME);
+        println!("        {} ended eating", self.name);
     }
 
     fn think(&self) {
-        println!("{} is thinking", self.name);
-        thread::sleep(Duration::from_secs(3));
-        println!("{} after thinking said: Eureka!", self.name);
+        println!("        {} started thinking", self.name);
+        thread::sleep(THINK_TIME);
+        println!("        {} ended thinking", self.name);
+
         self.done();
     }
 }
@@ -57,7 +68,21 @@ struct Table {
 }
 
 fn main() {
-    let (tx, rx) = channel();
+    println!("Dinning philosophers problem");
+    println!("----------------------------");
+    println!("Table is empty");
+
+    let (tx, rx) = channel::<bool>();
+    let (tx_seat, rx_seat) = channel();
+
+    let philosophers = vec![
+        Philosopher::new("Plato", 0, 1, tx.clone()),
+        Philosopher::new("Socrtes", 1, 2, tx.clone()),
+        Philosopher::new("Aristotele", 2, 3, tx.clone()),
+        Philosopher::new("Pascal", 3, 4, tx.clone()),
+        Philosopher::new("Locke", 4, 0, tx.clone()),
+    ];
+
     let table = Arc::new(Table {
         forks: vec![
             Mutex::new(true),
@@ -67,32 +92,37 @@ fn main() {
             Mutex::new(true),
         ],
     });
-
-    let philosophers = vec![
-        Philosopher::new("Baruch Spinoza", 4, 0, tx.clone()),
-        Philosopher::new("Gilles Deluze", 0, 1, tx.clone()),
-        Philosopher::new("Karl Marks", 1, 2, tx.clone()),
-        Philosopher::new("Friedrich Nietzsche", 2, 3, tx.clone()),
-        Philosopher::new("Michael Foucault", 3, 4, tx.clone()),
-    ];
+    let event_number = philosophers.len() * EATING_COUNTER;
 
     let handles: Vec<_> = philosophers
         .into_iter()
         .map(|p| {
             let table = table.clone();
+            let tx = tx_seat.clone();
             thread::spawn(move || {
-                for _ in 0..3 {
+                tx.send(format!("{} is seated", p.name))
+                    .ok()
+                    .expect("Unable to send message to seat channel");
+                for i in 0..EATING_COUNTER {
+                    println!("    {} meal for {}", i + 1, p.name);
                     p.eat(&table);
                     p.think();
                 }
             })
         })
         .collect();
-    for _ in 0..15 {
-        rx.recv().unwrap();
+
+    rx_seat.recv().into_iter().for_each(|message| {
+        println!("{}", message);
+    });
+
+    for _ in 0..event_number {
+        rx.recv().ok().expect("Unable to receive done messages");
     }
 
     for handle in handles {
-        handle.join().ok().expect("Couldn't join threads");
+        handle.join().ok().expect("Unable to join threads");
     }
+
+    println!("Table is empty");
 }
